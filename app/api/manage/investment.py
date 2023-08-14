3812475f16442c7e0f7c9b35d4110aa091f709af
Oskalovlev <111913579+Oskalovlev@users.noreply.github.com>
2023-08-14 20:02:42 +0300

    v4

--- app/api/manage/investment.py
+++ app/api/manage/investment.py
@@ -1,20 +1,20 @@
 from datetime import datetime
-from typing import List
+from typing import List, Union
 
 from app.models import CharityProject, Donation, BaseModel
 from app.core.config import settings
 
 
 def close_donation_for_obj(
-    target: CharityProject | Donation
+    target: Union[CharityProject, Donation]
 ) -> None:
     target.fully_invested = True
     target.close_date = datetime.now()
 
 
 def investing(
-    target: CharityProject | Donation,
-    sources: List[CharityProject | Donation],
+    target: Union[CharityProject, Donation],
+    sources: List[Union[CharityProject, Donation]],
 ) -> List[BaseModel]:
 
     target.invested_amount = settings.ZERO
